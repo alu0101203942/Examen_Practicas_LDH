5ecd9925f1de43accf3e2181e1161b6e89485014
alu0101203942 <alu0101203942@ull.edu.es>
2022-01-28 11:03:32 +0000

    haciendo los test

---
+++ src/test/java/com/thealgorithms/datastructure/graphs/DijskstrasAlgorithmTest.java
@@ -0,0 +1,14 @@
+package com.thealgorithms.datastructures.graphs;;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DijkstraAlgorimthTest {
+
+    @Test
+    void mintest() {
+
+    }
+    
+}
